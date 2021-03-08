d71d1d02060258d0fb81683c455cd914d769acc1
MizunaNako <63634104+MizunaNako@users.noreply.github.com>
2021-03-08 23:23:35 +0800

    Update ExamplePlugin.cs

--- Mirai-CSharp.Example/ExamplePlugin.cs
+++ Mirai-CSharp.Example/ExamplePlugin.cs
@@ -10,13 +10,7 @@ namespace Mirai_CSharp.Example
                                  INewFriendApply,
                                  IGroupApply,
                                  IBotInvitedJoinGroup,
-                                 IDisconnected,
-                                 IBotKickedOut
-    {
-        public Task<bool> BotKickedOut(MiraiHttpSession session, IBotKickedOutEventArgs e)
-        {
-            ThreadPool.QueueUserWorkItem(new WaitCallback(KiraDX.KiraPlugin.Event_KickBot), new KiraDX.KiraPlugin.BotKickOutVar(session, e));
-            return Task.Run(()=> { return false; });
-        }
-    }
+                                 IDisconnected
+    { }
+      
 }
